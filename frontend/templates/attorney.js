fea264eaad3ea0bffcb1094a831fa255d5d7553f
Upendra Falak <2020.upendra.falak@ves.ac.in>
2024-02-10 19:18:14 +0530

    uploaded different templates

---
+++ frontend/templates/attorney.js
@@ -0,0 +1,73 @@
+export const powerOfAttorneyFormat = `
+<p>
+TO ALL TO WHOM THESE PRESENTS SHALL COME
+<br/><br/>
+Know all men by these presents that I {grantorName} s/o {fathersName} aged {grantorAge} yrs r/o {grantorAddress}, state as follows :-
+<br/><br/>
+Whereas I am personally unable to attend to the managerial and other affairs with respect to my property {propertyDescription}, so I, hereby nominate and appoint Shri/Smt {attorneyHolderName} R/o {attorneyHolderAddress} as my true and lawful Attorney to act for and on my behalf and I authorize and empower him to do the following acts, deeds and things on my behalf:-
+<br/><br/><br/><br/>
+
+1. To rent the aforementioned property by leave and license agreement.
+<br/><br/>
+2. To purchase the stamp, make, sign, execute and admit the execution of leave and license agreement and to appear before the sub registrar for the registration of the leave and license agreement of the aforesaid property.
+<br/><br/>
+3. To demand, collect the rent due on the aforementioned property.
+<br/><br/>
+4. To manage and control my aforesaid property including collection of monthly rents, from the licensees and issuance of proper stamped receipts acknowledging the rent received. 
+<br/><br/>
+5. To make applications, affidavits, documents etc., to the Govt. Departments and any other concerned authorities, required for the managing of the aforesaid property and to do all other acts, deeds and things in respect thereof.
+<br/><br/>
+6. To effect and carry out necessary repairs, additions, etc., in the said property as and when may be desired, and for this purpose obtain all the necessary permissions and/or sanctions, necessary from any appropriate authority.
+<br/><br/>
+7. To deal with Govt. departments and other local bodies for the purpose of any essential facilities or amenities required to be provided in the building. He can sign all papers and documents etc. for this purpose.
+<br/><br/>
+8. To pay all the taxes, Municipal levies and other taxes, which may be, required to be paid.
+<br/><br/>
+9. To file any objections with Govt. departments or other local body of Government for any purpose related with said property. 
+<br/><br/>
+10.To furnish the details of Licensee to concerned Police Station, to get No Objection from the local Police by appearing personally as and when required or called by the Police of concerned jurisdiction.
+<br/><br/>
+11. To pay maintenance charges to society and attend society meetings.
+<br/><br/>
+12. To engage any Advocate or Attorney for the purpose and or to appear for and represent our in all the courts, civil, criminal or revenue including labour tribunals, original, revisional or appellate, in any registration offices, and also to present appeals in any court, and also to accept services of all summons, notices and other processes of law.
+<br/><br/>
+13. AND GENERALLY TO DO ALL other acts, deeds and things, which my said attorneys may deem fit and proper for the maintenance, upkeep of my property.
+<br/><br/>
+14. This power of attorney is without any consideration.
+<br/><br/><br/>
+Provided that the said attorney shall not sell or transfer the ownership of the property to any person. Provided further that the said attorney shall keep true accounts of all activities performed by virtue of this power of attorney.
+<br/><br/><br/><br/>
+AND
+<br/><br/><br/><br/>
+I hereby agree and undertake to confirm and ratify all and whatsoever my said attorney shall do or purport to do by the virtue of this power of attorney. 
+<br/><br/><br/><br/>
+IN WITNESS WHERE OF this deed is signed by me at {grantorAddress} on {agreementDate}.
+<br/><br/><br/><br/>
+1. EXECUTANT :
+{executantName}
+<br/><br/><br/>
+
+2. ATTORNEY HOLDER
+<br/>
+
+“I hereby accept all the powers.” : 
+{attorneyHolderName}
+<br/><br/>
+
+
+
+WITNESSES:
+<br/><br/>
+
+
+1. {witnessOneName}
+<br/>
+{witnessOneAddress}
+<br/><br/>
+
+
+2. {witnessTwoName}
+<br/>
+{witnessTwoAddress}
+</p>
+`;
---
+++ frontend/templates/housesale.js
@@ -0,0 +1,60 @@
+export const houseSaleAgreementFormat = `
+<p>
+This Agreement of sale made at {agreementState} on {agreementDate}, between {vendorName} son of {vendorsFathersName} resident of {vendorAddress} hereinafter called the vendor of the ONE PART and {purchaserName} son of {purchasersFathersName} resident of {purchaserAddress} hereinafter called the purchaser of the OTHER PART.
+<br/><br/>
+WHEREAS the vendor is absolutely seized and possessed of or well and sufficiently entitled to the house more fully described in the Schedule hereunder:
+<br/><br/>
+AND WHEREAS,the vendor has agreed to sell his house to the purchaser on the terms and conditions hereafter set-forth.
+<br/><br/><br/><br/>
+NOW this Agreement Witnesseth as Follows
+<br/><br/>
+1. The vendor will sell and the purchaser will purchase that entire house {vendorAddress} more particularly described in the Schedule hereunder written at a price of Rs. {housePrice} free from all encumbrances.
+<br/><br/>
+2. The purchaser has paid a sum of Rs. {advanceAmount} as earnest money on {advanceAmountDate} (the receipt of which sum, the vendor hereby acknowledges) and the balance amount of consideration will be paid at the time of execution of conveyance deed.
+<br/><br/>
+3. The sale shall be completed within a period of {salePeriod} months from this date and it is hereby agreed that time is the essence of the contract.
+<br/><br/>
+4. The vendor shall submit the title deeds of the house in his possession or power to the purchaser's advocate within one week from the date of this agreement for investigation of title and the purchaser will intimate about his advocate's report within {verificationTime} days after delivery of title deeds to his advocate.
+<br/><br/>
+5. If the purchaser's Advocate gives the report that the vendor's title is not clear, the vendor shall refund the earnest money, without interest to the purchaser within {returnOfAdvance} days from the date of intimation about the advocate's report by the purchasers. If the vendor does not refund the earnest money within {returnOfAdvance} days from the date of intimation about the advocate's report, the vendor will be liable to pay interest @ {rateOfInterest} p.m. upto the date of repayment of earnest money.
+<br/><br/>
+6. The vendor declares that the sale of the house will be without encumbrances.
+<br/><br/>
+7. The vendor will hand over the vacant possession of the house on the execution and registration of conveyance deed.
+<br/><br/>
+8. If the purchaser commits breach of the agreement, the vendor shall be entitled to forfeit the earnest money paid by the purchaser to the vendor and the vendor will be at liberty to resell the property to any person.
+<br/><br/>
+9. It the vendor commits breach of the agreement, he shall be liable to refund earnest money, received by him and a sum of Rs. {penaltyAmount} by way of liquidated damages.
+<br/><br/>
+10. The vendor shall execute the conveyance deed in favour of the purchaser or his nominee as the purchaser may require, on receipt of the balance consideration.
+<br/><br/>
+11. The vendor shall at his own costs obtain clearance certificate under section 230A, Income tax Act, 1961 and other permissions required for the completion of the sale.
+<br/><br/>
+12. The expenses for, preparation of the conveyance deed, cost of stamp, registration charges and all other cut of pocket expenses shall be borne by the purchaser.
+<br/><br/>
+Schedule above referred to
+<br/><br/>
+IN WITNESS WHEREOF the parties have set their hands to this Agreement on the day and year first hereinabove written.
+<br/><br/><br/>
+Signed and delivered by 
+Shri {vendorName}
+<br/><br/>
+Signed and delivered by 
+Shri {purchaserName}
+<br/><br/>
+
+WITNESSES:
+<br/><br/>
+
+
+1. {witnessOneName}
+<br/>
+{witnessOneAddress}
+<br/><br/>
+
+
+2. {witnessTwoName}
+<br/>
+{witnessTwoAddress}
+</p>
+`;
